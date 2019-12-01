12cc538470ca7b5ae4e9b4e44358d3b2e1b1a6c9
Silence-Rain <daniel.s.mo503@gmail.com>
2019-12-01 18:44:04 -0500

    add redirect attack

---
+++ attacks/redirect/redirect.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(302, {
+    'Location': 'http://silence-rain.cn:9888?actual=1&type=redirect-302'
+  });
+  res.end("302 redirecting\n");
+}).listen(9777);
