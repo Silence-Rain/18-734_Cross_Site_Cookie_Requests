7c036efb211926470a0ebd036bd16eec8568f7c5
Silence-Rain <daniel.s.mo503@gmail.com>
2019-11-30 23:35:25 -0500

    add firefox support

--- browser.py
+++ browser.py
@@ -1,32 +1,47 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-options = webdriver.ChromeOptions()
 
 
-# proxy settings
-PROXY = "127.0.0.1:8080"
-options.add_argument('--proxy-server=%s' % PROXY)
+# Chrome
+# options = webdriver.ChromeOptions()
 
-# browser settings
-# options.add_argument("--enable-experimental-web-platform-features")
-# prefs = {"block_third_party_cookies": True}
-# options.add_experimental_option("prefs", prefs)
+# # proxy settings
+# PROXY = "127.0.0.1:8080"
+# options.add_argument('--proxy-server=%s' % PROXY)
 
-# extension settings
-# path = "extensions/%s.crx" % "Ghostery"
-# options.add_extension(path)
+# # browser settings
+# # options.add_argument("--enable-experimental-web-platform-features")
+# # prefs = {"block_third_party_cookies": True}
+# # options.add_experimental_option("prefs", prefs)
+
+# # extension settings
+# # path = "extensions/%s.crx" % "Ghostery"
+# # options.add_extension(path)
+
+# driver = webdriver.Chrome(chrome_options=options)
 
 
-driver = webdriver.Chrome(chrome_options=options)
+# Firefox
+profile = webdriver.FirefoxProfile()
+profile.set_preference("network.proxy.http", "localhost")
+profile.set_preference("network.proxy.http_port", 8080)
+profile.set_preference("network.proxy.type", 1)
+profile.set_preference("security.csp.enable", True)
 
-try:
-	# set cookie from 3rd party
-	driver.get("http://silence-rain.cn:9888")
-	# attack
-	driver.get('http://localhost:8888/html/index.html')
-	time.sleep(1)
+# profile.set_preference("network.cookie.cookieBehavior", 1)
 
-finally:
-	driver.close()
+profile.update_preferences()
+
+driver = webdriver.Firefox(firefox_profile=profile)
+
+
+
+
+# set cookie from 3rd party
+driver.get("http://silence-rain.cn:9888")
+# attack
+driver.get('http://localhost:8888/html/index.html')
+time.sleep(1)
+
+
+driver.close()
--- proxy.py
+++ proxy.py
@@ -1,10 +1,16 @@
 import mitmproxy.http
-from mitmproxy import ctx
+
+res = []
 
 def request(flow: mitmproxy.http.HTTPFlow):
 	# request has been initiated
 	if "http://silence-rain.cn:9888/?actual=1" in flow.request.url:
-		print(flow.request.cookies)
-	# request has not been initiated
-	else:
-		pass
+		# cookie is attached
+		if "leak_test" in flow.request.cookies:
+			print("cookie is attached")
+		# cookie is not attached
+		else:
+			print("cookie is not attached")
+	# # request has not been initiated
+	# else:
+	# 	print("request has not been initiated")
