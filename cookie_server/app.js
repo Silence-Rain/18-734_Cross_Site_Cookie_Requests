6b39ad6711b4553dfcf15c7d34b75462dfb9bac4
Silence-Rain <daniel.s.mo503@gmail.com>
2019-12-02 14:02:08 -0500

    update cookie settings

--- cookie_server/app.js
+++ cookie_server/app.js
@@ -2,7 +2,8 @@ const http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {
-    'Set-Cookie': 'leak_test=1;Expires=Wed, 13-Jan-2020 22:23:01 GMT'
+    'Set-Cookie': 'leak_test=1;Expires=Wed, 13-Jan-2020 22:23:01 GMT',
+    'Access-Control-Allow-Origin': '45.77.79.119'
   });
   res.end("cookie has been set\n");
 }).listen(9888);
