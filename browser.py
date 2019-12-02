521b35c6872237b19815b9414d3763924f71d532
Silence-Rain <daniel.s.mo503@gmail.com>
2019-12-02 14:01:27 -0500

    finalize experiment

--- browser.py
+++ browser.py
@@ -1,55 +1,58 @@
 import time
 from selenium import webdriver
 
+extensions = ["AdBlockPlus", "Adguard", "uBlockOrigin", "Ghostery", "PrivacyBadger"]
+attacks = ["html", "redirect", "js", "pdf", "headers"]
 
 # Chrome
-# options = webdriver.ChromeOptions()
-
-# # proxy settings
-# PROXY = "127.0.0.1:8080"
-# options.add_argument('--proxy-server=%s' % PROXY)
-
-# # browser settings
-# # options.add_argument("--enable-experimental-web-platform-features")
-# # prefs = {"block_third_party_cookies": True}
-# # options.add_experimental_option("prefs", prefs)
-
-# # extension settings
-# # path = "extensions/chrome/%s.crx" % "PrivacyBadger"
-# # options.add_extension(path)
-
-# # driver = webdriver.Chrome(chrome_options=options)
-
-
-# Firefox
-profile = webdriver.FirefoxProfile()
+options = webdriver.ChromeOptions()
 
 # proxy settings
-profile.set_preference("network.proxy.http", "localhost")
-profile.set_preference("network.proxy.http_port", 8080)
-profile.set_preference("network.proxy.type", 1)
-profile.set_preference("security.csp.enable", True)
+PROXY = "127.0.0.1:8080"
+options.add_argument('--proxy-server=%s' % PROXY)
 
 # browser settings
-# profile.set_preference("network.cookie.cookieBehavior", 1)
+# options.add_argument("--enable-experimental-web-platform-features")
+# prefs = {"block_third_party_cookies": True}
+# options.add_experimental_option("prefs", prefs)
 
 # extension settings
-path = "/Users/danielmo/学习/项目/Personal_Projects/18-734_Cross_Site_Cookie_Requests/extensions/firefox/%s.xpi" % "Ghostery"
-# profile.add_extension(path)
+path = "extensions/chrome/%s.crx" % extensions[3]
+options.add_extension(path)
 
+driver = webdriver.Chrome(chrome_options=options)
 
-profile.update_preferences()
+# # Firefox
+# profile = webdriver.FirefoxProfile()
 
-driver = webdriver.Firefox(firefox_profile=profile)
-driver.install_addon(path, temporary=True)
+# # proxy settings
+# profile.set_preference("network.proxy.http", "localhost")
+# profile.set_preference("network.proxy.http_port", 8080)
+# profile.set_preference("network.proxy.type", 1)
+# profile.set_preference("security.csp.enable", True)
 
+# # browser settings
+# # profile.set_preference("network.cookie.cookieBehavior", 1)
+# # profile.set_preference("privacy.trackingprotection.enabled", 1)
+
+# # extension settings
+# path = "/Users/danielmo/学习/项目/Personal_Projects/18-734_Cross_Site_Cookie_Requests/extensions/firefox/%s.xpi" % extensions[3]
+# # profile.add_extension(path)
+
+# profile.update_preferences()
+
+# driver = webdriver.Firefox(firefox_profile=profile)
+
+# driver.install_addon(path)
 
 
 # set cookie from 3rd party
 driver.get("http://silence-rain.cn:9888")
 # attack
-driver.get('http://localhost:8888/html/index.html')
-time.sleep(1)
-
+for i in range(4):
+	driver.get('http://45.77.79.119/%s/index.html' % attacks[i])	
+	time.sleep(2)
+driver.get("http://45.77.79.119:9777")
+time.sleep(2)
 
 driver.close()
--- proxy.py
+++ proxy.py
@@ -4,13 +4,20 @@ res = []
 
 def request(flow: mitmproxy.http.HTTPFlow):
 	# request has been initiated
-	if "http://silence-rain.cn:9888/?actual=1" in flow.request.url:
+	if "http://silence-rain.cn:9888/?ad_type=1" in flow.request.url:
+		t = flow.request.query["type"]
 		# cookie is attached
 		if "leak_test" in flow.request.cookies:
-			print("cookie is attached")
+			print("%s: cookie is attached" % t)
 		# cookie is not attached
 		else:
-			print("cookie is not attached")
+			print("%s: cookie is not attached" % t)
 	# # request has not been initiated
 	# else:
 	# 	print("request has not been initiated")
+
+def clientconnect(layer: mitmproxy.proxy.protocol.Layer):
+	pass
+
+def clientdisconnect(layer: mitmproxy.proxy.protocol.Layer):
+	pass
